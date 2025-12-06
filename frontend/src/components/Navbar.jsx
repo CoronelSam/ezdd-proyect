70881764d022c0c5f42363279cec7f803ccc8189
CoronelSam <samueldiaz504hn@gmail.com>
2025-12-06 17:50:55 -0600

    feat: actualizar favicon, agregar manifest y reemplazar logo por versión webp

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import logo from '../assets/images/logo.png';
+import logo from '../assets/images/logo.webp';
 import { useAuth } from '../hooks/useAuth';
 import { useCarrito } from '../hooks/useCarrito';
 
